ad3d3e7f055e5aa440ee11d14d5de268856c51d5
Ashiq-1052 <ashiqur6401@gmail.com>
2025-01-25 21:19:21 +0600

    newspaper url added

--- Web-Scraping/Part-1/web_scraping/2. requests-html.py
+++ Web-Scraping/Part-1/web_scraping/2. requests-html.py
@@ -73,7 +73,7 @@ def main():
     # render_javascript('https://example.com')
 
     print("\nExtracting information from a web page...")
-    extract_information('https://www.prothomalo.com/world/usa/xnjy4uqwpv')
+    extract_information('https://www.ittefaq.com.bd/politics')
 
 if __name__ == "__main__":
     main()
